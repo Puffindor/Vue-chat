96de6113919e3ba82d172479340cd08e1b2035f5
Ilya Mzierov <ilmiz2012@gmail.com>
2023-06-18 17:24:08 +0700

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
- transpileDependencies: true,
- publicPath: "/Vue-chat/",
+ publicPath: process.env.NODE_ENV === "production" ? "/Vue-chat/" : "/",
 });
